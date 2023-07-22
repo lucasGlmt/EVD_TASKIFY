95e5bb68fff5bddc480437ca088bb1585fcb2b1d
Lucas Guilleminot <lcs.glmt@gmail.com>
2023-07-22 11:12:28 +0200

    fix: changement html

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import TodoList from './components/TodoList.vue';
   <div class="header">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Taskify</a>
+        <a class="navbar-brand" href="#">Tasks</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
